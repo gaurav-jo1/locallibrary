9e2dd9fd29c8a120eebd24f2b1c432fd90896382
Gaurav Joshi <gj451026@gmail.com>
2022-09-09 18:36:59 +0530

    admin.py models added

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,23 @@
+from atexit import register
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
+admin.site.register(Genre)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
