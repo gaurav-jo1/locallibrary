36277ab2f621e18657d54c85e4e311a785d7dea1
Gaurav Joshi <gj451026@gmail.com>
2022-09-09 12:35:30 +0530

    Models bug fixed

--- backend/urls.py
+++ backend/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('/', RedirectView.as_view(url='catalog/')),
+    path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,3 @@
-from statistics import mode
 import uuid
 from django.db import models
 from django.urls import reverse
@@ -27,8 +26,8 @@ class Book(models.Model):
         return reverse("book_detail", args=[str(self.id)])
 
 
-class BookInstance(models.model):
-    id = models.UUIDField(primary_field=True, default=uuid.uuid.uuid4,help_text='Unique ID for this particular book across whole library')
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
