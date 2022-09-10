b1bf58fc8ab5f1775611107cd1ca55c95533a1f2
Gaurav Joshi <gj451026@gmail.com>
2022-09-10 11:36:33 +0530

    admin site edited

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,13 +11,31 @@ admin.site.register(Genre)
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title','author','display_genre')
+
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
