3732caf81315c7272a5e19510f6bc710092e5c61
Gaurav Joshi <gj451026@gmail.com>
2022-09-30 16:12:07 +0530

    pagination added to frontend

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,29 +1,33 @@
-import React, {useState} from 'react'
-import Navigation from '../components/Navigation'
+import React from "react";
+import Navigation from "../components/Navigation";
 import { useQuery } from "@tanstack/react-query";
-import { Link } from 'react-router-dom';
-import ReactLoading from 'react-loading';
+import { Link } from "react-router-dom";
+import ReactLoading from "react-loading";
+// import Pages from './Pages';
 
 // Styles
 import "../styles/AllBooks.scss";
+import { useState } from "react";
 
-const AllBooks = ({theme}) => {
-  const [parms, setParms] = useState(1)
-  const { data: books, isLoading, isError,refetch} = useQuery(["books"], () => {
-    return fetch(`http://127.0.0.1:8000/catalog/ApiBooksListView/?page=${parms}`).then((t) => t.json())
-  })
+const AllBooks = ({ theme }) => {
+  const [id, setId] = useState(
+    "http://127.0.0.1:8000/catalog/ApiBooksListView/"
+  );
 
-  if (isLoading) return <div className="Loading" ><ReactLoading /></div>;
+  const { data: books, isLoading, isError, } = useQuery(["books", id], () => {
+    return fetch(id).then((t) => t.json());
+  });
 
-  if (isError) return <h1>Error with request</h1>
+  if (isLoading)
+    return (
+      <div className="Loading"> <ReactLoading /> </div>
+    );
 
-  console.log(parms)
-  
+  if (isError) return <h1>Error with request</h1>;
+  console.log(books);
   return (
-    <div className='AllBooks_container'>
-      <div>
-        <Navigation />
-      </div>
+    <div className="AllBooks_container">
+      <div> <Navigation /> </div>
       <div>
         <h1>All Books</h1>
         <div>
@@ -32,21 +36,25 @@ const AllBooks = ({theme}) => {
               <div className={`book_detail-container ${theme}`} key={book.id}>
                 <ul>
                   <Link to={"/Book/" + book.id}>
-                    <li><p className='books_title_p'>{book.title}&nbsp;</p></li>
+                    <li> <p className="books_title_p">{book.title}&nbsp;</p> </li>
                   </Link>
-                  <p className='book_author-name'>&nbsp;({book.author})</p>
+                  <p className="book_author-name">&nbsp;({book.author})</p>
                 </ul>
               </div>
-            )
+            );
           })}
         </div>
-        <div className='book_detail-pagination'>
-          <button onClick={() => setParms(parms - 1, refetch() )}>Previos</button>
-          <button onClick={() => setParms(parms + 1, refetch() )}>Next</button>
+        <div className="book_detail-pagination">
+          {books.previous ? ( <button onClick={() => setId(books.previous)}>Previos</button>
+          ) : ( <button style={{ background: "rgba(135, 207, 235, 0.5)", border: "none" }}> Previos </button>
+          )}
+          {books.next ? ( <button onClick={() => setId(books.next)}>Next</button>
+          ) : (<button style={{ background: "rgba(135, 207, 235, 0.5)", border: "none" }} > Next</button>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllBooks
+export default AllBooks;
