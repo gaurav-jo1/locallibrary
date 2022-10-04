60b4220a8d1f0945bc884a4d7021bd836214ffd9
Gaurav Joshi <gj451026@gmail.com>
2022-10-04 23:43:50 +0530

    database changed for prodcution

--- backend/settings.py
+++ backend/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'locallibrary', 
-        'USER': 'postgres', 
-        'PASSWORD': 'd@nnypola',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
