39a7dfcf57426fb13a709338b5fbabc005e55870
Gaurav Joshi <gj451026@gmail.com>
2022-10-17 16:51:32 +0530

    JWT Auth added

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'catalog',
-    'userProfile'
+    'userProfile',
     # tools
     "corsheaders",
     'rest_framework',
--- userProfile/urls.py
+++ userProfile/urls.py
@@ -5,7 +5,6 @@ from . import views
 from rest_framework_simplejwt.views import ( TokenRefreshView, TokenObtainPairView)
 
 urlpatterns = [
-    path('', views.front, name='front'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
